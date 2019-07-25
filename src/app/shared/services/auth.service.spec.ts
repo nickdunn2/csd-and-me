5134ac82a0fb70baf98a59be5b6ede663aa66604
Nick Dunn <nick.dunn@flosports.tv>
2019-07-25 09:39:40 -0500

    still very WIP with AuthService unit tests

--- src/app/shared/services/auth.service.spec.ts
+++ src/app/shared/services/auth.service.spec.ts
@@ -2,11 +2,11 @@ import { TestBed } from '@angular/core/testing'
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 
-import { AuthService } from './auth.service'
+import { AuthService, ILoginCredentials, IUser } from './auth.service'
 
 fdescribe(AuthService.name, () => {
-  let service: AuthService
-  let httpMock: HttpTestingController
+  // let service: AuthService
+  // let httpMock: HttpTestingController
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,19 +17,62 @@ fdescribe(AuthService.name, () => {
       providers: [AuthService]
     })
 
-    httpMock = TestBed.get(HttpTestingController)
-    service = TestBed.get(AuthService)
+    // httpMock = TestBed.get(HttpTestingController)
+    // service = TestBed.get(AuthService)
   })
 
-  afterEach(() => {
-    httpMock.verify()
+  // afterEach(() => {
+  //   httpMock.verify()
+  // })
+
+  describe(':', () => {
+    function setup() {
+      const authService = TestBed.get(AuthService)
+      const httpTestingController = TestBed.get(HttpTestingController)
+      return { authService, httpTestingController }
+    }
+
+    it('should be created', () => {
+      const { authService } = setup()
+      expect(authService).toBeTruthy()
+    })
+
+    // TODO: This test is not hitting the .subscribe block yet
+    it('should POST', () => {
+      const { authService, httpTestingController } = setup()
+      authService.login().subscribe(res => {
+        console.log('in here')
+        expect(false).toBeTruthy()
+        expect(res).toEqual('foobar')
+      })
+
+      const req = httpTestingController.expectOne('https://staging-api.flosports.tv/api/tokens')
+
+      expect(req.request.method).toBe('POST')
+
+      req.flush(mockLoginCreds)
+    })
+
+    afterEach(() => {
+      const { httpTestingController } = setup()
+      httpTestingController.verify()
+    })
+
   })
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  })
+  // it('should be ccreated', () => {
+  //   expect(service).toBeTruthy();
+  // })
 
-  it('should return POST blah blah ', () => {
+  // it('should return POST blah blah ', () => {
 
-  })
+  // })
 })
+
+const mockLoginCreds: ILoginCredentials = { email: 'foo@bar.com', password: 'foobar' }
+const mockAdminUser: IUser = {
+  email: 'foo@bar.com',
+  id: 23,
+  roles: ['ROLE_USER', 'ROLE_ADMIN'],
+  universal: true
+}
